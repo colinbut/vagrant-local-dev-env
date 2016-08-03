16794e4ce2818708b45f990563d24b969da09b46
Colin But <colinbut@users.noreply.github.com>
2016-08-03 22:30:03 +0100

    fixed OS issue with the vagrantfile

--- local-ci-jenkins/vagrant_provision.sh
+++ local-ci-jenkins/vagrant_provision.sh
@@ -37,8 +37,10 @@ then
 	# Start/Stop: /etc/init.d/jenkins
 	# Config: /etc/default/jenkins
 	# Jenkins log: /var/log/jenkins/jenkins.log
-	wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-	sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+	#wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+	#sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+	sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+	sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 	sudo yum update
 	sudo yum install -y jenkins
 
