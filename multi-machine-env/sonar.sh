8474f9ee7624a2efbe260240533dc2e287ab88ac
Colin But <colinbut@users.noreply.github.com>
2016-08-09 22:29:31 +0100

    provisioning scripts for multi-machine environment

---
+++ multi-machine-env/sonar.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# upating yum first
+sudo yum update
+
+# installing 'Extra Packages for Enterprise Linux'
+# yum package manager does not have all latest software on its default repository
+sudo yum install -y epel-release
+sudo yum install -y wget
+sudo yum install -y unzip
+
+#Java
+if [ ! -f /opt/jdk1.7.0_79/bin/java ];
+then
+	echo "Downloading Java 7"
+	cd /opt/
+	wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz"
+	tar xzf jdk-7u79-linux-x64.tar.gz
+	echo "Installing Java 7"
+	cd /opt/jdk1.7.0_79/
+	alternatives --install /usr/bin/java java /opt/jdk1.7.0_79/bin/java 2
+	alternatives --install /usr/bin/jar jar /opt/jdk1.7.0_79/bin/jar 2
+	alternatives --install /usr/bin/javac javac /opt/jdk1.7.0_79/bin/javac 2
+ 	alternatives --set jar /opt/jdk1.7.0_79/bin/jar
+ 	alternatives --set javac /opt/jdk1.7.0_79/bin/javac
+else
+	echo "Java 7 already installed - skipping"
+fi
+
+
+echo "Downloading Sonar"
+wget https://sonarsource.bintray.com/Distribution/sonarqube/sonarqube-5.5.zip
+
+echo "Unpacking Sonar"
+sudo unzip sonarqube-5.5.zip
+
+echo "Installing Sonar"
+sudo mv -v sonarqube-5.5 /opt/sonar
+
+echo "Starting Sonar Server"
+sudo /opt/sonar/bin/linux-x86-64/sonar.sh start
